31d7ab2d92332772d18248fe03dc839fe0e0edf0
NHNSang <nguyenhoangnhatsang01061998@gmail.com>
2023-11-22 16:13:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import DoiKinh from "./component/DoiKinh/DoiKinh";
+import Overley from "./component/Overlay/Overley";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Overley></Overley>
+      <DoiKinh></DoiKinh>
     </div>
   );
 }
---
+++ src/component/Overlay/Overley.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Overley extends Component {
+    
+  render() {
+    return (
+      <div className='overley'>
+      </div>
+    )
+  }
+}
